8d2168048aade8671174f67f5108a2fd2e6397f7
ritakassiane <ritakassiane.t.i@gmail.com>
2023-02-22 15:30:26 -0300

    :sparkles: Add: Componente SideBar, /components/Header

---
+++ src/components/Header/SideBar.tsx
@@ -0,0 +1,36 @@
+import Drawer from '@mui/material/Drawer';
+
+export interface SideBarProps{
+    open: boolean;
+    onClose: () => void;
+    items: [];
+}
+export function SideBar(props:SideBarProps){
+    return (
+        <Drawer open={props.open} onClose={props.onClose}>
+            <div className="bg-black w-64 p-4">
+                <h1 className="text-lg font-bold mb-4">Minha Sidebar</h1>
+                <nav>
+                    <ul>
+                        <li>
+                            <a href="www.google.com" className="text-gray-600 hover:text-gray-800">
+                                Link 1
+                            </a>
+                        </li>
+                        <li>
+                            <a href="www.google.com" className="text-gray-600 hover:text-gray-800">
+                                Link 2
+                            </a>
+                        </li>
+                        <li>
+                            <a href="www.google.com" className="text-gray-600 hover:text-gray-800">
+                                Link 3
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </Drawer>
+    )
+
+}
