09049d018fdaa4d8381fdac6b5b38a1d718fb8a6
ritakassiane <ritakassiane.t.i@gmail.com>
2023-02-22 12:26:51 -0300

    :sparkles: Add: Estilos padroes, cores e fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,32 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        sans: 'Archivo, sans-serif'
+      }
+    },
+    fontFamily:{
+      xs: 14,
+      sm: 16,
+      md: 18,
+      lg: 20,
+      xl: 24,
+      '2xl': 32,
+      '4xl': 48,
+    },
+
+    colors:{
+      transparent: 'transparent',
+      white: '#FFF',
+      black: '#000',
+      blacklight: '#00000010', 
+      blackdark: '#00000070', 
+      blackmedium: '#00000040', 
+      whitelight: '#FFFFFF10', 
+      whitedark: '#FFFFFF70', 
+      whitemedium: '#FFFFFF40', 
+    }
   },
   plugins: [],
 }
