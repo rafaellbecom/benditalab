4cf483e975cf28587c196bc593a74b422da31507
ritakassiane <ritakassiane.t.i@gmail.com>
2023-03-07 23:34:21 -0300

    :lipstick: Edit: Ajuste de lista do sidebar, /components/SideBar

--- src/components/Header/SideBar.tsx
+++ src/components/Header/SideBar.tsx
@@ -8,26 +8,32 @@ export interface SideBarProps{
 export function SideBar(props:SideBarProps){
     return (
         <Drawer open={props.open} onClose={props.onClose}>
-            <div className="bg-black w-[80vw] h-[100vh] font-bold p-4 text-white">
-                <h1 className="text-lg font-bold mb-4">Minha Sidebar</h1>
-                <nav>
-                    <ul>
-                        <li>
-                            <a href="www.google.com" className="text-gray-600 hover:text-gray-800">
-                                Link 1
-                            </a>
-                        </li>
-                        <li>
-                            <a href="www.google.com" className="text-gray-600 hover:text-gray-800">
-                                Link 2
-                            </a>
-                        </li>
-                        <li>
-                            <a href="www.google.com" className="text-gray-600 hover:text-gray-800">
-                                Link 3
-                            </a>
-                        </li>
-                    </ul>
+            <div className="bg-black w-[80vw] h-[100vh] font-semibold font-sans bold p-4 text-white text-6xl">
+                <nav className="flex items-end">
+                    <div className="items">
+                        <ul>
+                            <li className='mt-5'>
+                                <a href="www.google.com" className="text-gray-600 hover:text-gray-800">
+                                    HOME
+                                </a>
+                            </li>
+                            <li className='mt-5'>
+                                <a href="www.google.com" className="text-gray-600 hover:text-gray-800">
+                                    CONTA
+                                </a>
+                            </li>
+                            <li className='mt-5'>
+                                <a href="www.google.com" className="text-gray-600 hover:text-gray-800">
+                                    CARRINHO
+                                </a>
+                            </li>
+                            <li className='mt-5'>
+                                <a href="www.google.com" className="text-gray-600 hover:text-gray-800">
+                                    CAMISETA
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
                 </nav>
             </div>
         </Drawer>
