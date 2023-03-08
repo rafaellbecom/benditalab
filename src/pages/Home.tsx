23d022d32b3019c8c4c309f4dd4548cebb4aba13
Rafael Lima <rafaellbecom@gmail.com>
2023-03-07 21:32:10 -0300

    :sparkles: add: Footer, /componentes/Footer.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,14 @@
 import { Header } from "../components/Header/Header";
 import { Destaque } from "../components/Destaque";
 import { Slider } from "../components/Slider";
+import { Footer } from "../components/Footer";
 export function Home(){
     return(
         <div className="flex flex-col">
             <Header></Header>
             <Destaque></Destaque>
             <Slider></Slider>
+            <Footer></Footer>
         </div>
     )
 }
