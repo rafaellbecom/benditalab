9b6186a105797a744a7b8f4d3d1f4727bd3fa1f6
Rafael Lima <rafaellbecom@gmail.com>
2023-03-09 15:59:39 -0300

    :sparkles: add: Page SignIn, /pages/SignIn.tsx

---
+++ src/pages/SignIn.tsx
@@ -0,0 +1,49 @@
+import { Header } from "../components/Header/Header";
+import { Footer } from "../components/Footer";
+import ArrowBack from '@mui/icons-material/ArrowBackIosNew';
+
+export function SignIn(){
+    return(
+        <main>
+            <Header></Header>
+            <div className="grid grid-cols-5 h-[800px]">
+                <div className="col-form col-span-2 h-full flex items-center">
+                    <div className="w-[80%] mx-auto">
+                        <button className="w-[50px] h-[50px] mb-[32px]"><ArrowBack className="text-lg"></ArrowBack></button>
+
+                        <div className="formulario flex flex-col justify-center items-center gap-[32px]">
+                            <h1 className="font-sans font-bold text-3xl uppercase">Acesse sua conta</h1>
+                        
+                            <form action="" className="w-full flex flex-col justify-center items-center gap-[16px]">
+                                <input type="text" name="email" id="email" placeholder="Email" className="w-full px-[32px] py-[16px] border-2 border-blackmedium rounded-[8px] font-sans text-xl text-blackmedium"/>
+
+                                <input type="password" name="senha" id="senha" placeholder="Senha" className="w-full px-[32px] py-[16px] border-2 border-blackmedium rounded-[8px] font-sans text-xl text-blackmedium"/>
+
+                                <button className="w-full px-[32px] py-[16px] bg-black border-2 border-black rounded-[8px] font-sans font-bold text-xl text-white uppercase">Entrar</button>
+
+                                <a href="/signin" className="font-sans text-center text-black text-sm">Esqueci minha senha</a>
+                            </form>
+
+                            <div className="w-full flex justify-center items-center gap-[16px]">
+                                <div className="w-full h-[1px] bg-black"></div>
+                                <span className="font-sans text-center text-black text-sm">OU</span>
+                                <div className="w-full h-[1px] bg-black"></div>
+                            </div>
+
+                            <div className="auth-with flex justify-center items-center">
+                                <div className="auth-google w-[60px] h-[60px] bg-[url('../assets/login/auth-google.png')] bg-cover bg-center"></div>
+
+                                <div className="auth-facebook auth-google w-[60px] h-[60px] bg-[url('../assets/login/auth-facebook.png')] bg-cover bg-center"></div>
+                            </div>
+
+                            <button className="w-full px-[32px] py-[16px] border-2 border-black rounded-[8px] font-sans font-bold text-xl text-black uppercase">Criar conta</button>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col-span-3 h-full bg-[url('../assets/login/img-signin.jpg')] bg-cover bg-bottom"></div>
+            </div>
+            <Footer></Footer>
+        </main>
+    )
+}
--- src/routes/RoutesBase.tsx
+++ src/routes/RoutesBase.tsx
@@ -1,12 +1,13 @@
 import {BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Home } from '../pages/Home';
+import { SignIn } from '../pages/SignIn';
 
 export const RoutesBase = () => {
     return(
      <BrowserRouter>
          <Routes>
              <Route path="/" element={<Home/>} />
-
+             <Route path="/signin" element={<SignIn/>} />
          </Routes>
       </BrowserRouter>
     )
