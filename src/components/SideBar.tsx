e4ae6a77f6168d41aeb197bd7ace2bd6d133b5ba
Rafael Lima <rafaellbecom@gmail.com>
2023-03-27 18:55:41 -0300

    :lipstick: Edit: Sidebar collections, /components/SideBar.tsx

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -5,12 +5,17 @@ export function SideBar(){
         <main className="main side-bar w-1/3 bg-blacklight p-[32px] rounded-r-[8px]">
             <div>
                 <h3 className="title-side-bar">Coleção:</h3>
-
+                <div className="collections w-full flex flex-wrap justify-between items-center">
+                    <button className="collection">Start</button>
+                    <button className="collection">Bendita's</button>
+                    <button className="collection">Marijuana</button>
+                    <button className="collection">Smoking Club</button>
+                </div>
             </div>
 
             <div>
                 <h3 className="title-side-bar">Tamanho:</h3>
-                <div className="sizes w-full bg-white rounded-[8px] px-[24px] py-[18px] flex justify-around items-center">
+                <div className="sizes w-full bg-white rounded-[8px] px-[24px] py-[18px] flex justify-between items-center">
                     <button className="size">P</button>
                     <button className="size">PP</button>
                     <button className="size">M</button>
@@ -21,7 +26,7 @@ export function SideBar(){
 
             <div>
                 <h3 className="title-side-bar">Cor:</h3>
-                <div className="colors w-full flex justify-between items-center">
+                <div className="colors w-full flex flex-wrap justify-between items-center">
                     <button className="color bg-black"></button>
                     <button className="color bg-white"></button>
                     <button className="color bg-purple"></button>
