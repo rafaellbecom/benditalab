25113f278e90a20f3760b4564c95cf25d2a5fc0a
ritakassiane <ritakassiane.t.i@gmail.com>
2023-04-05 01:11:04 -0300

    :recycle: Refact: Semantica ideal, div -> section, limpeza de lixo visual, padronizaçao de espaçamentos, /[componentes relacionados a pag Home]

--- src/components/CarouselProduct.tsx
+++ src/components/CarouselProduct.tsx
@@ -7,31 +7,36 @@ import 'swiper/css/scrollbar';
 import { Product } from './Product';
 export function CarouselProduct (){
   return (
-    <div className="main container w-[100vw] h-[100vh] flex items-center justify-center overflow-hidden">
-            <div className="swiper-button-prev"></div>
-            <div className="swiper-button-next"></div>
-            <Swiper
-            
-              modules={[Pagination, Navigation]}
-              slidesPerView={3}
-              spaceBetween={50}
-              navigation
-              pagination={{
-                clickable: true,
-                }}
-              className="mySwiper"
-            >
-              <SwiperSlide className="w-[100vw]">
-                <Product></Product>
-              </SwiperSlide>
-              <SwiperSlide>
-                <Product></Product>
-              </SwiperSlide>
-              <SwiperSlide>
-                <Product></Product>
-              </SwiperSlide>
-            </Swiper>
-            <div className="swiper-navigation-container"></div>
-    </div>
+    <section className="main main w-[100vw] h-[100vh] flex flex-col justify-center items-center">
+      <div className="container my-[50px] w-[100vw] h-[90vh] flex items-center justify-center overflow-hidden">
+              <div className="swiper-button-prev"></div>
+              <div className="swiper-button-next"></div>
+              <Swiper
+              
+                modules={[Pagination, Navigation]}
+                slidesPerView={3}
+                spaceBetween={50}
+                navigation
+                pagination={{
+                  clickable: true,
+                  }}
+                className="mySwiper"
+              >
+                <SwiperSlide className="w-[100vw]">
+                  <Product></Product>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <Product></Product>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <Product></Product>
+                </SwiperSlide>
+              </Swiper>
+              <div className="swiper-navigation-container"></div>
+      </div>
+      <div className="text-container">    
+        <p className="scrolling-text font-semibold italic">    Frete Grátis usando o cupom “FRETEGRATIS”    Frete Grátis usando o cupom “FRETEGRATIS”    Frete Grátis usando o cupom “FRETEGRATIS”    Frete Grátis usando o cupom “FRETEGRATIS”    Frete Grátis usando o cupom “FRETEGRATIS”    Frete Grátis usando o cupom “FRETEGRATIS”    Frete Grátis usando o cupom “FRETEGRATIS”    Frete Grátis usando o cupom “FRETEGRATIS”    Frete Grátis usando o cupom “FRETEGRATIS”    Frete Grátis usando o cupom “FRETEGRATIS”       Frete Grátis usando o cupom “FRETEGRATIS”    Frete Grátis usando o cupom “FRETEGRATIS”    Frete Grátis usando o cupom “FRETEGRATIS”    Frete Grátis usando o cupom “FRETEGRATIS”    Frete Grátis usando o cupom “FRETEGRATIS”    Frete Grátis usando o cupom “FRETEGRATIS”    Frete Grátis usando o cupom “FRETEGRATIS”</p>
+      </div>
+    </section>
   );
 };
--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -5,32 +5,36 @@ import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
 import { Timer } from './Timer';
-import { Button } from './Utils/Button';
 export function Slider (){
   return (
-    <div className="main container w-[100vw] h-[90vh] flex items-center justify-center overflow-hidden">
-            <Swiper
-            modules={[Navigation, Pagination, Scrollbar, A11y]}
-            spaceBetween={100}
-            slidesPerView={1}
-            navigation
-            pagination={{ clickable: true }}
-            onSlideChange={() => console.log('slide change')}
-            onSwiper={(swiper) => console.log(swiper)}
-            >
-              <SwiperSlide className="w-[100vw] h-[60vh] rounded-sm px-[100px] py-[50px] flex flex-row flex-nowrap justify-center bg-blacklight bg-[url('../assets/home/banner-timer-background.png')]">
-                <div className="w-[50%] font-sans text-white">
-                  <h1 className="uppercase text-7xl  text-center font-bold">BENDITA SMOKING CLUB</h1>
-                  <p className='uppercase text-xl font-normal text-center mt-2'>30 PRIMEIRAS PEÇAS COM 30% OFF</p>
-                </div>
+    <section className="main w-[100vw] h-[100vh] flex flex-col justify-center items-center">
+      <div className="container my-[50px] w-[100vw] h-[90vh] flex flex-row items-center justify-center overflow-hidden">
+        <Swiper
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        spaceBetween={100}
+        slidesPerView={1}
+        navigation
+        pagination={{ clickable: true }}
+        onSlideChange={() => console.log('slide change')}
+        onSwiper={(swiper) => console.log(swiper)}
+        >
+          <SwiperSlide className="w-[100vw] h-[60vh] rounded-sm px-[100px] py-[50px] flex flex-row flex-nowrap justify-center bg-blacklight bg-[url('../assets/home/banner-timer-background.png')]">
+            <div className="w-[50%] font-sans text-white">
+              <h1 className="uppercase text-7xl  text-center font-bold">BENDITA SMOKING CLUB</h1>
+              <p className='uppercase text-xl font-normal text-center mt-2'>30 PRIMEIRAS PEÇAS COM 30% OFF</p>
+            </div>
 
-                <div className="w-[50%] flex flex-col items-center justify-center font-sans text-white">
-                  <Timer initialTime={1000}></Timer>
-                  <button className="mt-4 rounded-[8px] bg-white  px-8 py-4 text-black font-bold">COMPRAR NA PRE-VENDA</button>
-                </div>
-              </SwiperSlide>
-              <SwiperSlide>Slide 2</SwiperSlide>
-            </Swiper>
-    </div>
+            <div className="w-[50%] flex flex-col items-center justify-center font-sans text-white">
+              <Timer initialTime={1000}></Timer>
+              <button className="mt-4 rounded-[8px] bg-white  px-8 py-4 text-black font-bold">COMPRAR NA PRE-VENDA</button>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>Slide 2</SwiperSlide>
+        </Swiper>
+      </div>
+      <div className="text-container">    
+        <p className="scrolling-text font-semibold italic">    Frete Grátis usando o cupom “FRETEGRATIS”    Frete Grátis usando o cupom “FRETEGRATIS”    Frete Grátis usando o cupom “FRETEGRATIS”    Frete Grátis usando o cupom “FRETEGRATIS”    Frete Grátis usando o cupom “FRETEGRATIS”    Frete Grátis usando o cupom “FRETEGRATIS”    Frete Grátis usando o cupom “FRETEGRATIS”    Frete Grátis usando o cupom “FRETEGRATIS”    Frete Grátis usando o cupom “FRETEGRATIS”    Frete Grátis usando o cupom “FRETEGRATIS”       Frete Grátis usando o cupom “FRETEGRATIS”    Frete Grátis usando o cupom “FRETEGRATIS”    Frete Grátis usando o cupom “FRETEGRATIS”    Frete Grátis usando o cupom “FRETEGRATIS”    Frete Grátis usando o cupom “FRETEGRATIS”    Frete Grátis usando o cupom “FRETEGRATIS”    Frete Grátis usando o cupom “FRETEGRATIS”</p>
+      </div>
+    </section>
   );
 };
