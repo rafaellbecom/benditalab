100cba10cfde5ae93e35ddc752b08092839ded01
ritakassiane <ritakassiane.t.i@gmail.com>
2023-04-05 01:53:59 -0300

    :sparkles:(WIP) Add: Single Product Page

---
+++ src/pages/SingleProduct.tsx
@@ -0,0 +1,13 @@
+export function SingleProduct (){
+    return (
+        <section className="product">
+            {/* 
+              - Galeria
+              - Single Product Details (Nome do produto, preço, cor, tamanho... etc)
+              - Single Product Descriçao
+              - Drop banner 
+            
+            */}
+        </section>
+    )
+}
