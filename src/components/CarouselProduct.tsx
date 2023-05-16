f8af9c2658f3ad98d8c8edacca776778d312e55c
Rafael Lima <rafaellbecom@gmail.com>
2023-05-16 17:26:19 -0300

    :lipstick: Corrigindo imagem do Product e Seção de Destaques na HOME, /components/Product.tsx and /components/CarouselProduct.tsx

--- src/components/CarouselProduct.tsx
+++ src/components/CarouselProduct.tsx
@@ -7,13 +7,13 @@ import 'swiper/css/scrollbar';
 import { Product } from './Product';
 export function CarouselProduct (){
   return (
-    <section className="main main w-[100vw] h-[100vh] my-[50px] flex flex-col justify-center">
-      <div className="title container px-[90px] flex h-[10vh] items-center">
+    <section className="main w-[100vw] h-auto mt-[45px] flex flex-col justify-center">
+      <div className="title container flex h-[10vh] items-center">
         <h1 className="uppercase text-5xl  text-left font-bold">DESTAQUES</h1>
-        <button className="mt-4 ml-4 rounded-[8px] bg-transparent border-solid border-blackmediu border-[2px] font-medium px-6 py-[9px] text-[15px] text-blackmedium">#ESTILOSMAISQUENTES</button>
+        <button className="ml-4 rounded-[8px] bg-transparent border-solid border-blackmedium border-[2px] font-medium px-6 py-[9px] text-[15px] text-blackmedium">#ESTILOSMAISQUENTES</button>
       </div>
       <div className="products flex w-[100vw] justify-center items-center my-[30px]">
-        <div className="container w-[100vw] h-[80vh] flex items-center justify-center overflow-hidden">
+        <div className="container h-auto flex items-center justify-center overflow-hidden">
           <div className="swiper-button-prev"></div>
           <div className="swiper-button-next"></div>
           <Swiper
@@ -40,7 +40,7 @@ export function CarouselProduct (){
           <div className="swiper-navigation-container"></div>
         </div>
       </div>
-      <div className="text-container mt-[-50px] h-[90px]">
+      <div className="text-container h-[70px] mt-[45px]">
         <div className="text-only">
           <p className="scrolling-text font-semibold italic">    Frete Grátis usando o cupom “FRETEGRATIS”    Frete Grátis usando o cupom “FRETEGRATIS”    Frete Grátis usando o cupom “FRETEGRATIS”    Frete Grátis usando o cupom “FRETEGRATIS”    Frete Grátis usando o cupom “FRETEGRATIS”    Frete Grátis usando o cupom “FRETEGRATIS”    Frete Grátis usando o cupom “FRETEGRATIS”    Frete Grátis usando o cupom “FRETEGRATIS”    Frete Grátis usando o cupom “FRETEGRATIS”    Frete Grátis usando o cupom “FRETEGRATIS”       Frete Grátis usando o cupom “FRETEGRATIS”    Frete Grátis usando o cupom “FRETEGRATIS”    Frete Grátis usando o cupom “FRETEGRATIS”    Frete Grátis usando o cupom “FRETEGRATIS”    Frete Grátis usando o cupom “FRETEGRATIS”    Frete Grátis usando o cupom “FRETEGRATIS”    Frete Grátis usando o cupom “FRETEGRATIS”</p>
         </div>    
--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -3,16 +3,16 @@ import React from 'react';
 
 export function Product(){
     return (
-        <div className="product flex flex-col flex-1 w-[100%] h-[80vh]">
-            <div className="product-image w-full h-[455px] bg-[url('../assets/product/imagem-product.jpg')] bg-cover bg-no-repeat bg-center p-[15px]">
+        <div className="product flex flex-col w-full h-auto">
+            <div className="product-image w-full h-[400px] bg-[url('../assets/product/imagem-product.jpg')] bg-cover bg-no-repeat bg-center p-[15px]">
                 <Tag></Tag>
             </div>
 
-            <div className="product-content">
-                <h1 className="product-title font-sans font-bold uppercase text-lg mt-[15px] text-blackdark ">START - Camiseta bulls</h1>
+            <div className="product-content mt-[15px]">
+                <h1 className="product-title font-sans font-bold uppercase text-lg text-blackdark ">START - Camiseta bulls</h1>
                 
-                <div className="price-product">
-                    <span className="installments font-sans font-regular text-sm mt-[15px] text-black">6x de 18,32 sem juros</span>
+                <div className="price-product mt-[15px]">
+                    <span className="installments font-sans font-regular text-sm text-black">6x de 18,32 sem juros</span>
                     <h2 className="value font-sans font-bold text-2xl">R$ 109,90</h2>
                 </div>
             </div>
